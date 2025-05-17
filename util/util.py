af1bd675d698900bbfad2809fb9702684de8724e
MarinaDistler <marina_distler@mail.ru>
2025-05-17 16:38:50 +0000

    revrt changes in promt

--- util/util.py
+++ util/util.py
@@ -8,7 +8,7 @@ import shutil
 from transformers import TrainerCallback
 import ast
 
-from util.promts import get_train_prompt
+from util.promts import get_train_prompt, system_instruction
 
 def print_options(opt, parser):
     message = ''
@@ -71,6 +71,7 @@ class ChatGenerationCallback(TrainerCallback):
             
             user_prompt = get_train_prompt(row['input'], row['rhyme_scheme'], row['meter'])
             messages = [
+                {"role": "system", "content": system_instruction},
                 {"role": "user", "content": user_prompt}
             ]
             text = self.tokenizer.apply_chat_template(
